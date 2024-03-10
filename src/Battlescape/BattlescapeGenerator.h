ce42483b7e7021811cbb5b8cd698d4d8af07f73f
ohartenstein23 <ohartenstein23@gmail.com>
2024-03-09 20:21:01 -0500

    Refactored loading of maps from the FileBrowser, still needs more validation

--- src/Battlescape/BattlescapeGenerator.h
+++ src/Battlescape/BattlescapeGenerator.h
@@ -204,7 +204,8 @@ public:
 	static void autoEquip(std::vector<BattleUnit*> units, Mod *mod, std::vector<BattleItem*> *craftInv,
 		RuleInventory *groundRuleInv, int worldShade, bool allowAutoLoadout, bool overrideEquipmentLayout);
 	/// Sets up a single map block for editing.
-	void loadMapForEditing(MapBlock *block, std::string filePath = "");
+	void loadMapForEditing();
+	void loadEmptyMap();
 };
 
 }
